46d88eaf3b65386aad7c8f471e437a2b9f29ea58
BalbinoSilva <everton16.b@gmail.com>
2019-01-27 12:53:05 -0200

    troca de repositorio

---
+++ src/main/java/com/myhairproject/client/MyHairProjectApplication.java
@@ -0,0 +1,13 @@
+package com.myhairproject.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHairProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHairProjectApplication.class, args);
+	}
+}
+
