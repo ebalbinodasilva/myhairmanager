a67dd27696d6aac3e1c9f70c38401898d4ef31d5
BalbinoSilva <everton16.b@gmail.com>
2019-01-28 18:08:31 -0200

    alteração de estrutura de aplicação

--- src/main/java/com/myhairproject/client/services/MyHairProjectServices.java
+++
@@ -1,9 +0,0 @@
-package com.myhairproject.client.services;
-
-import com.myhairproject.client.dao.MyHairProjectDao;
-
-public interface MyHairProjectServices {
-	MyHairProjectDao getById(Integer id);
-	MyHairProjectDao save(MyHairProjectDao myhairprojectdao);
-
-}
